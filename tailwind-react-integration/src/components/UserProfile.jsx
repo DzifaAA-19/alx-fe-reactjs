7fe06b1fdb7e86055734a8960918fd0cf251e4bd
DzifaAA-19 <agbakpedzifaaku3@gmail.com>
2026-02-08 20:41:25 +0000

    Adding styles to UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
+    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
       <img 
         src="https://via.placeholder.com/150" 
         alt="User" 
-        className="w-24 h-24 md:w-36 md:h-36 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
+        className="w-24 h-24 sm:w-36 sm:h-36 mx-auto rounded-full hover:scale-110 transition-transform duration-300 ease-in-out"
       />
       <h1 className="text-lg sm:text-xl text-blue-800 my-4 hover:text-blue-500 transition-colors duration-300">
         John Doe
